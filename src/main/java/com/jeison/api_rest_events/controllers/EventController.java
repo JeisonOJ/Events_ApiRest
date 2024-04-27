b9b5c05858f5ab546481bf498fb9f72d1f1a369f
Jeison <jeisonortiz1516@gmail.com>
2024-04-27 16:44:19 -0500

    RestController

--- src/main/java/com/jeison/api_rest_events/controllers/EventController.java
+++ src/main/java/com/jeison/api_rest_events/controllers/EventController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +14,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jeison.api_rest_events.models.Event;
 import com.jeison.api_rest_events.services.IEventService;
 
-@Controller
+@RestController
 @RequestMapping("api/event")
 public class EventController {
 
